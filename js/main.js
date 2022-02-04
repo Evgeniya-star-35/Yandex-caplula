3d8b3c17120812f73a1bff5aca0e25fdbd2262ba
Evgeniya Bayda <baje75128@gmail.com>
2022-02-05 00:40:20 +0200

    add

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ if ($("#slider").length) {
   $("#slider").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 3000,
     dots: true,
     adaptiveHeight: true,
@@ -12,21 +12,28 @@ if ($("#slider").length) {
       {
         breakpoint: 1200,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
           dots: true,
         },
       },
       {
-        breakpoint: 768,
+        breakpoint: 992,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
         },
       },
       {
-        breakpoint: 480,
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 320,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
