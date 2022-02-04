0bf614039e3391fa234eb7895c666c73540a98f4
Evgeniya Bayda <baje75128@gmail.com>
2022-02-04 10:55:02 +0200

    add slider

--- js/main.js
+++ js/main.js
@@ -1,10 +1,38 @@
 if ($("#slider").length) {
-  $(".slider").slick({
-    prevArrow:
-      '<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>',
-    nextArrow:
-      '<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>',
+  $("#slider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // autoplay: true,
+    autoplaySpeed: 3000,
     dots: true,
+    adaptiveHeight: true,
+    // infinite: false,
+
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 
   $(".slider").slick("slickGoTo", 1);
--- js/rewiev.js
+++ js/rewiev.js
@@ -1,32 +1,37 @@
-$(".responsive").slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true,
+$(document).ready(function () {
+  $("#slider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    adaptiveHeight: true,
+    // infinite: false,
+
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
       },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
       },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
       },
-    },
-  ],
+    ],
+  });
 });
